6bdfac215c7c9e0eb8994b950ff77fb2d806a51d
Roman <honrom@Romans-iMac.local>
2013-01-16 17:10:57 +0100

    try to resolve problem of .xcuserstate-file

---
+++ IntoSeries/BonjourServicesDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BonjourServicesDelegate.h
+//  IntoSeries
+//
+//  Created by Roman on 16.01.13.
+//  Copyright (c) 2013 MrThrasher. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BonjourServicesDelegate : NSObject
+
+@end
---
+++ IntoSeries/BonjourServicesDelegate.m
@@ -0,0 +1,13 @@
+//
+//  BonjourServicesDelegate.m
+//  IntoSeries
+//
+//  Created by Roman on 16.01.13.
+//  Copyright (c) 2013 MrThrasher. All rights reserved.
+//
+
+#import "BonjourServicesDelegate.h"
+
+@implementation BonjourServicesDelegate
+
+@end
